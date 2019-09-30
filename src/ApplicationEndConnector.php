114be95fe6833981407dc42bfa0d07a9d864a4e2
Jose Lorente <jose.lorente.martin@gmail.com>
2019-09-30 19:46:43 +0200

    * Included license in package files.

--- src/ApplicationEndConnector.php
+++ src/ApplicationEndConnector.php
@@ -1,11 +1,28 @@
 <?php
 
+/**
+ * Part of the Laravel Application End Queue package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Laravel Application End Queue
+ * @version    1.0.2
+ * @author     Jose Lorente
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2018, Jose Lorente
+ */
+
 namespace Jlorente\Laravel\Queue\ApplicationEnd;
 
 use Illuminate\Queue\Connectors\ConnectorInterface;
 
 /**
- * Class QueueServiceProvider
+ * Class ApplicationEndConnector
  *
  * @author Jose Lorente <jose.lorente.martin@gmail.com>
  */
--- src/ApplicationEndWorker.php
+++ src/ApplicationEndWorker.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * Part of the Laravel Application End Queue package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Laravel Application End Queue
+ * @version    1.0.2
+ * @author     Jose Lorente
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2018, Jose Lorente
+ */
+
 namespace Jlorente\Laravel\Queue\ApplicationEnd;
 
 use Illuminate\Contracts\Debug\ExceptionHandler;
